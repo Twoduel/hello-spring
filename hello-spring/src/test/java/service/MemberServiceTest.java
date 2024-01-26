9b3c6be2b7c747609c5d525a2191ec3aca2e5cf2
KimSujeong <sujeong0619.b@gmail.com>
2024-01-26 20:30:09 +0900

    회원 서비스 테스트

---
+++ hello-spring/src/test/java/service/MemberServiceTest.java
@@ -0,0 +1,36 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+
+    @Test
+    void 회원 가입() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    void findMember() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    void findOne() {
+        //given
+
+        //when
+
+        //then
+    }
+}
