4eec1e9e3b2611fc5a424fe1cf9df48003c18e33
KimSujeong <sujeong0619.b@gmail.com>
2024-01-28 22:16:28 +0900

    웹 기능 - 회원 등록.

--- hello-spring/src/main/java/example/com/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/example/com/hellospring/controller/MemberController.java
@@ -1,7 +1,10 @@
 package example.com.hellospring.controller;
 
+import example.com.hellospring.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import service.MemberService;
 
 @Controller
@@ -9,7 +12,23 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
